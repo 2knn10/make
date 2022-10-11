b8f8cc395a1017fcd66c45814c99c1b4833b5ddc
2knn10 <109735963+2knn10@users.noreply.github.com>
2022-10-11 14:06:18 +0300

    Update make pass.py

--- make pass.py
+++ make pass.py
@@ -27,8 +27,23 @@ y = "[10]#(*+{}=/@!%^<>,:;$"
 all=A + a + n + y
 length = 8
 password ="".join(random.sample(all,length))
-print("your password :",password)
-        
-        
+password1 ="".join(random.sample(all,length))
+password2 ="".join(random.sample(all,length))
+password3 ="".join(random.sample(all,length))
+password4 ="".join(random.sample(all,length))
+print("your password :",password)      
 md5  = hashlib.md5(password.encode())
 print(md5.hexdigest())
+print("your password :",password1)         
+md5  = hashlib.md5(password1.encode())
+print(md5.hexdigest())  
+print("your password :",password2)       
+md5  = hashlib.md5(password2.encode())
+print(md5.hexdigest())
+print("your password :",password3) 
+md5  = hashlib.md5(password3.encode())
+print(md5.hexdigest())  
+print("your password :",password4)
+md5  = hashlib.md5(password4.encode())
+print(md5.hexdigest())
+  
